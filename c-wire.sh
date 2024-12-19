58f813081c9e9cf3de0647168d74d681bdfefa2b
_kza.a <167125328+hfsaa@users.noreply.github.com>
2024-12-19 09:00:03 +0100

    Update c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -87,53 +87,52 @@ fi
 
 case "$station-$consumption" in
     "hvb-company")
-        timer awk -F";" '($2 != "-" && $5 != "-") || ($2 != "-" && $7 != "-") {print $2 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/hvbCtmp.csv
+        timer "awk -F";" '($2 != "-" && $5 != "-") || ($2 != "-" && $7 != "-") {print $2 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/hvbCtmp.csv"
         tmp_file="tmp/hvbCtmp.csv"#fichier temp non trié
-        sorted_tmp_file="tests/hvbCtmp_sorted.csv"#fichier trié
+        final_file="tests/hvb_comp.csv"
         ;; #hvb company
 
     "hva-company")
-        timer awk -F";" '($3 != "-" && $5 != "-") || ($3 != "-" && $7 != "-") {print $3 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/hvaCtmp.csv
+        timer "awk -F";" '($3 != "-" && $5 != "-") || ($3 != "-" && $7 != "-") {print $3 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/hvaCtmp.csv"
         tmp_file="tmp/hvaCtmp.csv"
-        sorted_tmp_file="tests/hvaCtmp_sorted.csv"
+        final_file="tests/hva_comp.csv"
         ;; #hva company
 
     "lv-company")
-        timer awk -F";" '($4 != "-" && $5 != "-") || ($4 != "-" && $7 != "-") {print $4 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/lvCtmp.csv
+        timer "awk -F";" '($4 != "-" && $5 != "-") || ($4 != "-" && $7 != "-") {print $4 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/lvCtmp.csv"
         tmp_file="tmp/lvCtmp.csv"
-        sorted_tmp_file="tests/lvCtmp_sorted.csv"
+        final_file="tests/lv_comp.csv"
         ;; #lv company
 
     "lv-individual")
-        timer awk -F";" '($4 != "-" && $6 != "-") || ($4 != "-" && $7 != "-") {print $4 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/lvItmp.csv
+        timer "awk -F";" '($4 != "-" && $6 != "-") || ($4 != "-" && $7 != "-") {print $4 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/lvItmp.csv"
         tmp_file="tmp/lvItmp.csv"
-        sorted_tmp_file="tests/lvItmp_sorted.csv"
+        final_file="tests/lv_indiv.csv"
         ;; #lv indiv
 
     "lv-all")
-        timer awk -F";" '($4 != "-" && $5 != "-" && $6 != "-") || ($4 != "-" && $7 != "-") {print $3 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/lvAtmp.csv
+        timer "awk -F";" '($4 != "-" && $5 != "-" && $6 != "-") || ($4 != "-" && $7 != "-") {print $3 ";" $7 ";" $8}' input/c-wire_v00.dat > tmp/lvAtmp.csv"
         tmp_file="tmp/lvAtmp.csv"
-        sorted_tmp_file="tests/lvAtmp_sorted.csv"
+        tmpminmax_file="tmp/lv_all.csv"
 
-        minmax_file="tests/lv_all_minmax.csv" #on crée un fichier additionnel en plus qui va stocker les 10 min et 10 max, minmax_file est une variable qui contient le chemin complet du nv fichier
-        echo "Nom;Capacite;Consommation;Difference" > "$minmax_file" #en-tête
+        final_file="tests/lv_all_minmax.csv" #on crée un fichier additionnel en plus qui va stocker les 10 min et 10 max, minmax_file est une variable qui contient le chemin complet du nv fichier
+        echo "Nom;Capacite;Consommation;Difference" > "$final_file" #en-tête
 
         awk -F";" 'NR > 1 { #awk divise chaque ligne en champs $1 $2 $3 grace au séparateur -F";" NR > 1 pour éviter la première ligne (en-tête)
             capacite = $2; 
             consommation = $3;
             difference = capacite - consommation;
             print $0 ";" difference; #On ajoute à la fin de la ligne $0 une nouvelle colonne contenant la différence calculée
-        }' "$final_file" | sort -t';' -k4,4n | { #on trie les données selon la 4ᵉ colonne (la colonne difference) : les lignes sont triées dans l'ordre croissant de la différence
-            head -n 10 >> "$minmax_file" #on ajoute les 10 lv max
-            tail -n 10 >> "$minmax_file" #on ajoute les 10 lv min
+        }' "$tmpminmax_file" | sort -t';' -k4,4n | { #on trie les données selon la 4ᵉ colonne (la colonne difference) : les lignes sont triées dans l'ordre croissant de la différence
+            timer "head -n 10 >> "$final_file"" #on ajoute les 10 lv max
+            timer "tail -n 10 >> "$final_file"" #on ajoute les 10 lv min
         }
 
-        echo "File $minmax_file has been successfully generated."
-        if [[ -f "$minmax_file" ]]; then #si le fichier a bien été créé
-            echo "Display $minmax_file:" #on veut afficher le contenu du fichier minmax
-            cat "$minmax_file"
+        echo "File $final_file has been successfully generated."
+        if [[ -f "$final_file" ]]; then #si le fichier a bien été créé
+            echo "Created directory"
         else
-            echo "Error: $minmax_file was not generated properly."
+            echo "Error: $final_file was not generated properly."
             exit 7
         fi
         ;;
@@ -144,9 +143,6 @@ case "$station-$consumption" in
         exit 6
 esac
 
-sort -t ';' -k 2,2n -k 2.1,2.1n "$tmp_file" > "$sorted_tmp_file" #tri du fichier temporaire
-echo "Sorted file created: $sorted_tmp_file" #sauvegarde dans un fichier trié
-
 # Création ou réinitialisation du fichier final
 if [[ ! -f "$final_file" ]]; then
     echo "Station; Capacity; Consumption" > "$final_file"  # Créer le fichier avec l'entête
@@ -156,3 +152,5 @@ else
 fi
 
 ./codeC/MNH_CWire "$sorted_tmp_file" "$final_file" #utilisation du fichier trié comme entrée pour le programme C
+
+timer "sort -t ';' -k 2,2 "$final_file""
